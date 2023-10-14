d4f1843a2fd447ce0d0d987c0a18c016d9dbcd94
Vishal Ravikumar <vr36@njit.edu>
2023-10-13 23:24:58 -0400

    Changed the message to hello world.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello from Dockerized Node.js!');
+    res.send('Hello World!');
 });
 
 app.listen(PORT, () => {
