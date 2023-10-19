ee36066dcd5da9676971a6c3a69b8d4b71e2f090
Benjamin Shuster <bentzishuster613@gmail.com>
2023-10-19 14:50:12 -0400

    documentation

--- gettingstarted/src/app/page.js
+++ gettingstarted/src/app/page.js
@@ -1,8 +1,8 @@
-import styles from './page.module.css'
+import styles from './page.module.css' // Import css modules stylesheet as styles
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
+  return ( 
+    <main className={styles.main}> 
       <h1 className={styles.title}>Hello World</h1>
     </main>
   )
