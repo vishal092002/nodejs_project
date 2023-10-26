e423172ae59bc7ecde1753da329a62f3fcb83fb3
Benjamin Shuster <77933963+bentzi-shuster@users.noreply.github.com>
2023-10-26 16:53:08 -0400

    basic auth0/JWT setup

--- project/src/app/layout.js
+++ project/src/app/layout.js
@@ -1,5 +1,5 @@
 import './globals.css'
-
+import { UserProvider } from '@auth0/nextjs-auth0/client';
 export const metadata = {
   title: 'Hello World', // title element, shown in the browser tab
   description: 'This is a description of my website', // description meta tag, for search engines
@@ -8,7 +8,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+       <UserProvider>  {/* This is needed for Auth0 to work*/}
       <body>{children}</body>
+      </UserProvider>
     </html>
   )
 }
--- project/src/app/page.js
+++ project/src/app/page.js
@@ -1,9 +1,23 @@
 import styles from './page.module.css' // Import css modules stylesheet as styles
-
-export default function Home() {
+import { getSession } from "@auth0/nextjs-auth0";
+export default async function Home() {
+  const session = await getSession();
+  let loggedIn = session && session.user && session.user["name"];
   return ( 
     <main className={styles.main}> 
-      <h1 className={styles.title}>Hello World</h1>
+      <h1 className={styles.title}>Hello {loggedIn ? session.user["name"] : "World"}!</h1>
+      <a href={loggedIn ? "/api/auth/logout" : "/api/auth/login"}>{loggedIn ? "Logout" : "Login"}</a>
+      {loggedIn && (<>
+      
+
+      <h3 className={styles.JWT} >JWT:</h3>
+      <pre className={styles.JWT}>
+  {(session?.idToken)}
+</pre>      
+</>
+      )}
+
+   
     </main>
   )
 }
